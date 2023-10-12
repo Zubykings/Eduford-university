f6128c5c8dbbf91c3c694dec434b5c01a9968be9
Zubykings <zubykings@gmail.com>
2023-10-12 17:39:31 +0100

    firstCommit

---
+++ form-handler.php
@@ -0,0 +1,13 @@
+<?php
+$name = $_POST['name']
+$visitor_email = $_POST['email']
+$subject = $_POST['subject']
+$message = $_POST['message']
+
+$email_from = 'zubykings@gmail.com'
+
+$email_subject = 'New Form Submission'
+
+$email_body = "User Name: $name"
+
+?>
